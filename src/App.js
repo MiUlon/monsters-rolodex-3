908108cdb82fa3e34979f68ae0a0543d4d41451b
Mindaugas Ulonas <mindegi15@gmail.com>
2023-05-12 14:25:16 +0300

    creating monsters

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: {
-        firstName: 'MindaugasFirstState',
-        lastName: 'MindaugasLastState'
-      },
-      company: 'MiU freelancerState'
+      monsters: [
+        {
+          name: 'Mindaugas0'
+        },
+        {
+          name: 'Mindaugas1'
+        },
+        {
+          name: 'Mindauags2'
+        },
+        {
+          name: 'Mindaugas3'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi, my name is { this.state.name.firstName } { this.state.name.lastName }! I work at company { this.state.company }.</p>
-          <button onClick={
-            () => {
-              this.setState( () => {
-                return {
-                  name: {
-                    firstName: 'MindaugasFirstOnClick',
-                    lastName: 'MindaugasLastOnClick'
-                  },
-                  company: 'MiU freelancerOnClick'
-                }
-              },
-              () => { console.log(this.state) })
-            }
-          }
-          >Change name and company</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{ monster.name }</h1>
+          })
+        }
       </div>
     );
   }
